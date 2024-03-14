9e9687ec596026090e065ab10dd142bc5a5d70c0
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-14 16:42:37 +0500

    Created new file Order

---
+++ frontend/src/components/pages/Order.js
@@ -0,0 +1,182 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getOrderDetails } from "../../redux/action/orderAction";
+import { ColorRing } from "react-loader-spinner";
+import { loadStripe } from "@stripe/stripe-js";
+
+const Order = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { loading, order, error } = orderDetails;
+  useEffect(() => {
+    if (!order || order._id !== id) {
+      dispatch(getOrderDetails(id));
+    }
+  }, [order, id]);
+
+  const payOrderHandler = async () => {
+    const stripe = await loadStripe(process.env.STRIPE_PUBLISHED_KEY);
+  };
+
+  return loading ? (
+    <div className="items-center flex justify-center">
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+    </div>
+  ) : error ? (
+    <h1>{error}</h1>
+  ) : (
+    <>
+      <div>
+        <div className="flex flex-col gap-4 mx-4">
+          <h1 className="text-xl font-bold">Order: {order._id}</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <div>
+                <h1 className="text-lg font-semibold">Shipping Address</h1>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-slate-600 leading-5">
+                  Name:{order.user.name}
+                </p>
+                <p className="text-sm text-slate-600 leading-5">
+                  <a
+                    href={`mailto:${order.user.email}`}
+                    className="hover:underline"
+                  >
+                    Email:{order.user.email}
+                  </a>
+                </p>
+                <p className="text-sm text-slate-600 leading-5">
+                  Address: {order.shippingAddress.address},{" "}
+                  {order.shippingAddress.city},{" "}
+                  {order.shippingAddress.postalCode},{" "}
+                  {order.shippingAddress.country}
+                </p>
+                <p>
+                  {order.isDelivered ? (
+                    <p className="bg-green-400 text-green-500 text-xs font-bold px-4 py-4 w-full">
+                      Delivered at:{order.deliveredAt}
+                    </p>
+                  ) : (
+                    <p className="bg-red-400 text-red-500 text-xs  font-bold px-4 py-4 w-full">
+                      Not Delivered
+                    </p>
+                  )}
+                </p>
+              </div>
+            </div>
+            <hr className="my-2" />
+            <div className="flex flex-col gap-2">
+              <div>
+                <h1 className="text-lg font-semibold">Payment Method</h1>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-slate-600 leading-5">
+                  Payment Method: {order.paymentMethod}
+                </p>
+                <p>
+                  {order.isPaid ? (
+                    <p className="bg-green-400 text-green-500 text-xs font-bold px-4 py-4 w-full">
+                      Paid on:{order.paidAt}
+                    </p>
+                  ) : (
+                    <p className="bg-red-400 text-red-500 text-xs  font-bold px-4 py-4 w-full">
+                      Not Paid
+                    </p>
+                  )}
+                </p>
+              </div>
+            </div>
+            <hr className="my-2" />
+            <div className="flex flex-col gap-2">
+              <div>
+                <h1 className="text-lg font-semibold">Ordered Items</h1>
+              </div>
+              <div>
+                {order.orderItems.map((item, index) => (
+                  <div
+                    className="flex flex-row px-3 py-3 gap-3 items-center"
+                    key={index}
+                  >
+                    <div className="">
+                      <img
+                        src={item.image}
+                        className="w-20 h-20 object-contain"
+                      ></img>
+                    </div>
+                    <div className=" flex flex-col gap-4">
+                      <div className="flex flex-row justify-between gap-2">
+                        <Link to={`/products/${item.product}`}>
+                          <h1 className="text-sm text-slate-600 leading-5 font-semibold">
+                            {item.name}
+                          </h1>
+                        </Link>
+                      </div>
+                      <div className="flex flex-row justify-between">
+                        <p className="text-sm text-slate-600 leading-5 font-semibold">
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="flex flex-col gap-4 py-[16px] px-[14px] border-[1px] border-black rounded-[20px]">
+                <div>
+                  <h1 className="text-xl font-bold">Order Sammary</h1>
+                </div>
+                <div className="flex flex-col gap-4">
+                  <div className="flex flex-row justify-between">
+                    <p className="text-black/[60%] text-[16px]">SubTotal</p>
+                    <p className="text-[16px] font-bold">${order.totalPrice}</p>
+                  </div>
+                  <div className="flex flex-row justify-between">
+                    <p className="text-black/[60%] text-[16px]">Delivery Fee</p>
+                    <p className="text-[16px] font-bold">
+                      ${order.shippingPrice}
+                    </p>
+                  </div>
+                  <hr />
+                  <div className="flex flex-row justify-between">
+                    <p className="text-black/[60%] text-[16px]">Total</p>
+                    <p className="text-[16px] font-bold">${order.totalPrice}</p>
+                  </div>
+                </div>
+                <button
+                  className="text-white bg-black py-4 rounded-full w-full"
+                  onClick={payOrderHandler}
+                  disabled={order.orderItems.length === 0}
+                >
+                  Pay {order.totalPrice}
+                  <span>
+                    <i
+                      className="fa-solid fa-arrow-right-long"
+                      style={{ color: "white" }}
+                    ></i>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Order;
