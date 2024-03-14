1eddc37ec797f89ffe4f287bfaad4341bd63d037
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-14 11:20:44 +0500

    Updated index file with Order route

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const dontenv = require("dotenv")
-const connectDb = require("./config/db")
-const productRoutes = require("./routes/productRoutes")
-const UserRoutes = require("./routes/userRoutes")
+const express = require("express");
+const dontenv = require("dotenv");
+const connectDb = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+const UserRoutes = require("./routes/userRoutes");
+const OrderRoutes = require("./routes/orderRoutes");
 // const { default: products } = require("./products")
 
+dontenv.config();
+connectDb();
+const app = express();
+app.use(express.json());
 
-dontenv.config()
-connectDb()
-const app = express()
-app.use(express.json())
+app.use("/api/products", productRoutes);
+app.use("/api/users", UserRoutes);
+app.use("/api/orders", OrderRoutes);
+const PORT = process.env.PORT;
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',UserRoutes)
-const PORT = process.env.PORT
-
-app.listen(PORT,console.log(`server is running on port ${PORT}`))
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
