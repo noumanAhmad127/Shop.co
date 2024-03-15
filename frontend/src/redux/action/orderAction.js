aeeb11182f9ec81906407682009819f31744d258
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-15 16:58:27 +0500

    Added ListMyOrder Action

--- frontend/src/redux/action/orderAction.js
+++ frontend/src/redux/action/orderAction.js
@@ -105,3 +105,37 @@ export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
     });
   }
 };
+
+export const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: actionTypes.ORDER_LIST_MY_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/myorders`, config);
+    console.log(data);
+
+    dispatch({
+      type: actionTypes.ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
