9484c841d1f12f6edb1936ea7cbcaf05cc8edf59
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-15 16:56:58 +0500

    updated

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,11 +4,13 @@ const {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } = require("../controller/orderController");
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
-router.route("/:id/pay").get(protect, updateOrderToPaid);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 module.exports = router;
