b66fd2cc7e479913feca1ae7a61b7fb7b28e5359
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-15 16:57:57 +0500

    updated Profile and Added Orders Lists

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {
   UpdateUserProfile,
   getUserDetail,
 } from "../../redux/action/userAction";
+import { listMyOrders } from "../../redux/action/orderAction";
+import { ColorRing } from "react-loader-spinner";
 
 const Profile = () => {
   const [name, setName] = useState("");
@@ -19,11 +21,13 @@ const Profile = () => {
   const userDetail = useSelector((state) => state.userDetail);
   const userLogin = useSelector((state) => state.userLogin);
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const orderMyList = useSelector((state) => state.orderMyList);
 
   const { loading, error, user } = userDetail;
   const { userInfo } = userLogin;
   const { success } = userUpdateProfile;
   // dispatch(login({ email, password }));
+  const { loading: loadingOrders, error: errorOrders, orders } = orderMyList;
 
   useEffect(() => {
     if (!userInfo) {
@@ -32,6 +36,7 @@ const Profile = () => {
 
     if (!user.name) {
       dispatch(getUserDetail("profile"));
+      dispatch(listMyOrders());
     } else {
       setName(user.name);
       setEmail(user.email);
@@ -47,7 +52,7 @@ const Profile = () => {
     }
   };
   return (
-    <div className="flex flex-col justify-between mx-6 my-6">
+    <div className="flex flex-col justify-between mx-6 my-6 gap-5">
       <div className="flex flex-col gap-10">
         {error && (
           <h1 className="text-center bg-red-300 text-red-600 text-sm py-4 w-full">
@@ -123,9 +128,94 @@ const Profile = () => {
           </form>
         </div>
       </div>
-      <div className="flex">
+      <hr className="w-full my-4" />
+      <div className="flex flex-col gap-4">
         <div>
-          <h1>My Orders</h1>
+          <h1 className="text-3xl font-medium">My Orders</h1>
+        </div>
+        <div>
+          {loadingOrders ? (
+            <div className="items-center flex justify-center">
+              <ColorRing
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="color-ring-loading"
+                wrapperStyle={{}}
+                wrapperClass="color-ring-wrapper"
+                colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+              />
+            </div>
+          ) : errorOrders ? (
+            <h1>{error}</h1>
+          ) : (
+            <div class="relative overflow-x-auto">
+              <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="col" class="px-6 py-3">
+                      Order id
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      date
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      total price
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      paid
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      delivered
+                    </th>
+                    <th scope="col" class="px-6 py-3"></th>
+                  </tr>
+                </thead>
+
+                {orders.map((order) => (
+                  <tbody>
+                    <tr class="bg-white border-b">
+                      <th
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {order._id}
+                      </th>
+                      <td class="px-6 py-4">
+                        {order.createdAt.substring(0, 10)}
+                      </td>
+                      <td class="px-6 py-4">{order.totalPrice}</td>
+                      {order.isPaid ? (
+                        <td class="px-6 py-4 bg-green-200 text-green-400">
+                          Paid
+                        </td>
+                      ) : (
+                        <td class="px-6 py-4  bg-red-200 text-red-400">
+                          {" "}
+                          Not Paid
+                        </td>
+                      )}
+                      {order.isDelivered ? (
+                        <td class="px-6 py-4 bg-green-200 text-green-400">
+                          Delivered
+                        </td>
+                      ) : (
+                        <td class="px-6 py-4  bg-red-200 text-red-400">
+                          {" "}
+                          Not Delivered
+                        </td>
+                      )}
+                      <td class="px-6 py-4">
+                        <Link to={`/order/${order._id}`} className="underline">
+                          Details
+                        </Link>
+                      </td>
+                    </tr>
+                  </tbody>
+                ))}
+              </table>
+            </div>
+          )}
         </div>
       </div>
     </div>
