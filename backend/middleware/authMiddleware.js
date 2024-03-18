edd052baa10519322b7430f67b6dd776482f61c6
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-18 16:10:23 +0500

    Updated authMiddleware
    added admin middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
@@ -10,7 +10,7 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.headers.authorization.split(" ")[1];
       const decode = jwt.verify(token, process.env.SECRET_KEY);
       console.log(decode);
       req.user = await User.findById(decode.id).select("-password");
@@ -27,4 +27,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = protect;
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).json({ message: "Not Authorized as Admin" });
+    throw new Error("Not Authorized as Admin");
+  }
+};
+
+module.exports = { protect, admin };
