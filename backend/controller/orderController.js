b819f990e2927e5f1ccf6375da886d0bff62410e
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-19 16:55:29 +0500

    Updated Order Controller
    added getOrder controller

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -76,9 +76,15 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.status(201).json(orders);
 });
 
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.status(201).json(orders);
+});
+
 module.exports = {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
 };
