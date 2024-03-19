bbbac1577f339c092ccafad7d3887d6a4bda9371
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-19 16:59:46 +0500

    Added new product Actions

--- frontend/src/redux/action/productAction.js
+++ frontend/src/redux/action/productAction.js
@@ -1,46 +1,139 @@
-import axios from "axios"
-import { actionTypes } from "./action-types"
+import axios from "axios";
+import { actionTypes } from "./action-types";
 // import actionTypes from "./action-types"
 
+export const listProduct = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: actionTypes.PRODUCT_LIST_REQUEST,
+    });
 
-export const listProduct= () => async(dispatch)=> {
-        try {
-            dispatch({
-                type:actionTypes.PRODUCT_LIST_REQUEST
-            })
+    const { data } = await axios.get("/api/products");
 
-            const { data } = await axios.get('/api/products')
+    dispatch({
+      type: actionTypes.PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PRODUCT_LIST_FAIL,
+      payload: error,
+    });
+  }
+};
 
-            dispatch({
-                type:actionTypes.PRODUCT_LIST_SUCCESS,
-                payload:data
-            })
-        } catch (error) {
-            dispatch({
-                type:actionTypes.PRODUCT_LIST_FAIL,
-                payload:error
-            })
-            
-        }
-}
+export const detailProduct = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: actionTypes.PRODUCT_DETAIL_REQUEST,
+    });
 
-export const detailProduct= (id) => async(dispatch)=> {
-        try {
-            dispatch({
-                type:actionTypes.PRODUCT_DETAIL_REQUEST
-            })
+    const { data } = await axios.get(`/api/products/${id}`);
 
-            const { data } = await axios.get(`/api/products/${id}`)
+    dispatch({
+      type: actionTypes.PRODUCT_DETAIL_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PRODUCT_DETAIL_FAIL,
+      payload: error,
+    });
+  }
+};
 
-            dispatch({
-                type:actionTypes.PRODUCT_DETAIL_SUCCESS,
-                payload:data
-            })
-        } catch (error) {
-            dispatch({
-                type:actionTypes.PRODUCT_DETAIL_FAIL,
-                payload:error
-            })
-            
-        }
-}
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: actionTypes.PRODUCT_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.delete(`/api/products/${id}`, config);
+
+    dispatch({
+      type: actionTypes.PRODUCT_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PRODUCT_DELETE_FAIL,
+      payload: error,
+    });
+  }
+};
+
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: actionTypes.PRODUCT_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/products`, {}, config);
+
+    dispatch({
+      type: actionTypes.PRODUCT_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PRODUCT_CREATE_FAIL,
+      payload: error,
+    });
+  }
+};
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: actionTypes.PRODUCT_UPDATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/products/${product._id}`,
+      product,
+      config
+    );
+
+    dispatch({
+      type: actionTypes.PRODUCT_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.PRODUCT_UPDATE_FAIL,
+      payload: error,
+    });
+  }
+};
