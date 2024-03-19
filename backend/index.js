0ef161d93fe69cfafb01db97108beba0fb3032ec
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-19 16:56:06 +0500

    Updated Index file
    Added new routes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
+const path = require("path");
 const express = require("express");
 const dontenv = require("dotenv");
 const connectDb = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const UserRoutes = require("./routes/userRoutes");
 const OrderRoutes = require("./routes/orderRoutes");
+const UploadRoutes = require("./routes/uploadRoutes");
 // const { default: products } = require("./products")
 
 dontenv.config();
@@ -14,7 +16,12 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", UserRoutes);
 app.use("/api/orders", OrderRoutes);
+app.use("/api/upload", UploadRoutes);
+
 app.use("/api/config/stripe", (req, res) => process.env.STRIPE_PUBLISHED_KEY);
+
+var __dirname = path.resolve();
+app.use("/upload", express.static(path.join(__dirname, "/upload")));
 const PORT = process.env.PORT;
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
