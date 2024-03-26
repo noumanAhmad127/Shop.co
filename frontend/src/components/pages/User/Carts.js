ff8dfe4513fb17add065c2d65acfa1b1bbd040ea
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-26 12:42:07 +0500

    updated

--- frontend/src/components/pages/User/Carts.js
+++ frontend/src/components/pages/User/Carts.js
@@ -45,110 +45,105 @@ const Carts = () => {
     .reduce((acc, item) => acc + item.qty * item.price, 0)
     .toFixed(2);
 
-  const totalPrice = (parseFloat(cartItemPrice) + deliveryPrice).toFixed(2);
+  // const totalPrice = (parseFloat(cartItemPrice) + deliveryPrice).toFixed(2);
   return (
-    <div className="mx-4">
+    <div className="mx-4 xl:w-[85%] xl:mx-auto">
       <div className="mb-5">
         <h1 className="text-3xl font-bold">Your Cart</h1>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="border-[1px] border-black/[60%] rounded-[20px]">
-          {cartItems.length === 0 ? (
-            <h1 className="text-sm bg-red-500 text-red-700 w-full py-5 px-4">
-              Cart Is Empty
-            </h1>
-          ) : (
-            <>
-              {cartItems.map((item) => (
-                <div className="flex flex-row px-3 py-3 gap-3">
-                  <div className="basis-1/3">
-                    <img src={item.image}></img>
+      {cartItems.length === 0 ? (
+        <h1 className="text-sm bg-red-500 text-red-700 w-full py-5 px-4">
+          Cart Is Empty
+        </h1>
+      ) : (
+        <div className="flex flex-col gap-4 xl:flex-row">
+          <div className="border-[1px] border-black/[60%] rounded-[20px] xl:basis-[60%]">
+            {cartItems.map((item) => (
+              <div className="flex flex-row px-3 py-3 gap-3">
+                <div className="basis-1/3">
+                  <img src={item.image}></img>
+                </div>
+                <div className="basis-2/3 flex flex-col gap-4">
+                  <div className="flex flex-row justify-between gap-2">
+                    <h1 className="text-base font-bold">{item.name}</h1>
+                    <span>
+                      <button
+                        onClick={() => {
+                          removeFromCartHandler(item.productId);
+                        }}
+                      >
+                        <i
+                          class="fa-solid fa-trash-can "
+                          style={{ color: "red" }}
+                        ></i>
+                      </button>
+                    </span>
                   </div>
-                  <div className="basis-2/3 flex flex-col gap-4">
-                    <div className="flex flex-row justify-between gap-2">
-                      <h1 className="text-base font-bold">{item.name}</h1>
-                      <span>
-                        <button
-                          onClick={() => {
-                            removeFromCartHandler(item.productId);
-                          }}
+                  <div className="flex flex-row justify-between">
+                    <p className="text-xl font-bold">${item.price}</p>
+                    <div>
+                      <form className="flex gap-3 items-center">
+                        <select
+                          value={item.qty}
+                          onChange={(e) =>
+                            dispatch(
+                              addToCart(item.productId, Number(e.target.value))
+                            )
+                          }
+                          className="px-6 py-1 border-[1px] rounded-full text-center"
                         >
-                          <i
-                            class="fa-solid fa-trash-can "
-                            style={{ color: "red" }}
-                          ></i>
-                        </button>
-                      </span>
-                    </div>
-                    <div className="flex flex-row justify-between">
-                      <p className="text-xl font-bold">${item.price}</p>
-                      <div>
-                        <form className="flex gap-3 items-center">
-                          <select
-                            value={item.qty}
-                            onChange={(e) =>
-                              dispatch(
-                                addToCart(
-                                  item.productId,
-                                  Number(e.target.value)
-                                )
-                              )
-                            }
-                            className="px-6 py-1 border-[1px] rounded-full text-center"
-                          >
-                            {[...Array(item.countInStock).keys()].map((x) => (
-                              <option
-                                key={x + 1}
-                                value={x + 1}
-                                className="px-2 py-2"
-                              >
-                                {x + 1}
-                              </option>
-                            ))}
-                          </select>
-                        </form>
-                      </div>
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option
+                              key={x + 1}
+                              value={x + 1}
+                              className="px-2 py-2"
+                            >
+                              {x + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </form>
                     </div>
                   </div>
                 </div>
-              ))}
-            </>
-          )}
-        </div>
-        <div>
-          <div className="flex flex-col gap-4 py-[16px] px-[14px] border-[1px] border-black rounded-[20px]">
-            <div>
-              <h1 className="text-xl font-bold">
-                Subtotal (
-                {cartItems.reduce(
-                  (pervItem, currItem) => pervItem + currItem.qty,
-                  0
-                )}
-                ) Items
-              </h1>
-            </div>
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-row justify-between">
-                <p className="text-black/[60%] text-[16px]">SubTotal</p>
-                <p className="text-[16px] font-bold">${cartItemPrice}</p>
               </div>
+            ))}
+          </div>
+          <div className="xl:basis-[40%]">
+            <div className="flex flex-col gap-4 py-[16px] px-[14px] border-[1px] border-black rounded-[20px] ">
+              <div>
+                <h1 className="text-xl font-bold">
+                  Subtotal (
+                  {cartItems.reduce(
+                    (pervItem, currItem) => pervItem + currItem.qty,
+                    0
+                  )}
+                  ) Items
+                </h1>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-row justify-between">
+                  <p className="text-black/[60%] text-[16px]">SubTotal</p>
+                  <p className="text-[16px] font-bold">${cartItemPrice}</p>
+                </div>
+              </div>
+              <button
+                className="text-white bg-black py-4 rounded-full w-full"
+                onClick={checkoutHandler}
+                disabled={cartItems.length === 0}
+              >
+                Go To Checkout{" "}
+                <span>
+                  <i
+                    class="fa-solid fa-arrow-right-long"
+                    style={{ color: "white" }}
+                  ></i>
+                </span>
+              </button>
             </div>
-            <button
-              className="text-white bg-black py-4 rounded-full w-full"
-              onClick={checkoutHandler}
-              disabled={cartItems.length === 0}
-            >
-              Go To Checkout{" "}
-              <span>
-                <i
-                  class="fa-solid fa-arrow-right-long"
-                  style={{ color: "white" }}
-                ></i>
-              </span>
-            </button>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
