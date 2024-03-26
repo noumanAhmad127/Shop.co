b54966f51af4554e0a05af1e141fae3deb066915
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-26 12:44:02 +0500

    updated and modified

--- frontend/src/components/utils/Cards/ProductCards.js
+++ frontend/src/components/utils/Cards/ProductCards.js
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 
 const ProductCards = ({ products }) => {
   return (
-    <div className="w-[172px]">
+    <div className="w-[172px] xl:w-[295px]">
       <div className="flex flex-col gap-3">
         <div>
           <Link to={`/products/${products._id}`}>
             <img
               src={products.image}
-              className="w-[172px] h-[174px] rounded-[13.42px] bg-contain"
+              className="w-[172px] h-[174px] rounded-[13.42px] bg-contain xl:w-full xl:h-[298px]"
             ></img>
           </Link>
         </div>
         <div className="flex flex-col gap-1">
           <Link to={`/products/${products._id}`}>
-            <h1 className="text-base font-bold text-ellipsis hover:underline">
+            <h1 className="text-base font-bold text-ellipsis hover:underline xl:text-[20px]">
               {products.name}
             </h1>
           </Link>
@@ -25,11 +25,13 @@ const ProductCards = ({ products }) => {
               <Rating value={products.rating} />
             </div>
             <div>
-              <p className="text-[12px] font-normal">{products.rating}/5</p>
+              <p className="text-[12px] font-normal xl:text-[18.49px]">
+                {products.rating}/5
+              </p>
             </div>
           </div>
           <div>
-            <p className="text-xl font-bold">${products.price}</p>
+            <p className="text-xl font-bold xl:text-2xl">${products.price}</p>
           </div>
         </div>
       </div>
