50e5dd91e474fa8c65ed52fd18e919021c1c1305
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-26 12:43:53 +0500

    updated and modified

--- frontend/src/components/pages/User/Shipping.js
+++ frontend/src/components/pages/User/Shipping.js
@@ -33,67 +33,69 @@ const Shipping = () => {
   };
   return (
     <>
-      <CheckOutSteps step1 step2 />
-      <div className="flex flex-col justify-center mx-6 my-6">
-        <div className="hidden"></div>
-        <div className="flex flex-col gap-10">
-          <div className="flex flex-col gap-6">
-            <h1 className="text-3xl font-medium">Shipping</h1>
-            <p className="text-sm">Enter Your Shipping Detail</p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <form onSubmit={sumbitHandler} autoComplete="off">
-              <div className="flex flex-col gap-6">
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Enter Address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3 autofill:none"
-                    autoComplete="off"
-                  />
+      <div className="my-5 xl:my-10">
+        <CheckOutSteps step1 step2 />
+        <div className="flex flex-col justify-center mx-6 my-6 max-w-3xl md:mx-auto">
+          <div className="hidden"></div>
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-6">
+              <h1 className="text-3xl font-medium">Shipping</h1>
+              <p className="text-sm">Enter Your Shipping Detail</p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <form onSubmit={sumbitHandler} autoComplete="off">
+                <div className="flex flex-col gap-6">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Enter Address"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3 autofill:none"
+                      autoComplete="off"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Enter City"
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
+                      className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3 autofill:none"
+                      autoComplete="off"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Enter Postal Code"
+                      value={postalCode}
+                      onChange={(e) => setPostalCode(e.target.value)}
+                      className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3"
+                      autoComplete="off"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Enter Country"
+                      value={country}
+                      onChange={(e) => setCountry(e.target.value)}
+                      className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3"
+                      autoComplete="off"
+                    />
+                  </div>
+                  <div className="my-3">
+                    <button
+                      className="text-base text-white bg-black py-4 px-6 rounded-full"
+                      type="submit"
+                    >
+                      Continue
+                    </button>
+                  </div>
                 </div>
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Enter City"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3 autofill:none"
-                    autoComplete="off"
-                  />
-                </div>
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Enter Postal Code"
-                    value={postalCode}
-                    onChange={(e) => setPostalCode(e.target.value)}
-                    className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3"
-                    autoComplete="off"
-                  />
-                </div>
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Enter Country"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                    className="text-sm border-b-[1px] border-black/[60%] w-full px-1 py-3"
-                    autoComplete="off"
-                  />
-                </div>
-                <div className="my-3">
-                  <button
-                    className="text-base text-white bg-black py-4 px-6 rounded-full"
-                    type="submit"
-                  >
-                    Continue
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
