b172382ddfd9fdf15dd76409fac713f88341d768
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-03-27 15:20:29 +0500

    updated and modified

--- frontend/src/components/utils/Header.js
+++ frontend/src/components/utils/Header.js
@@ -27,9 +27,144 @@ const Header = () => {
   };
 
   return (
-    <div>
+    <div className=" shadow-lg">
       <div className="w-[90%] mx-auto py-5 xl:w-[85%]">
-        {userInfo && !userInfo.isAdmin ? (
+        {userInfo && userInfo.isAdmin ? (
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row gap-2 items-center">
+              <div>
+                <h1 className="text-[25.2px] font-bold text-black xl:text-[32px]">
+                  SHOP.CO
+                </h1>
+              </div>
+              <div className="hidden xl:block mx-10">
+                <div className="py-1 flex flex-row">
+                  <Link
+                    to="/admin/userlist"
+                    className="text-gray-700 block px-4 py-2 text-sm"
+                  >
+                    Users
+                  </Link>
+                  <Link
+                    to="/admin/productlist"
+                    className="text-gray-700 block px-4 py-2 text-sm"
+                  >
+                    Products
+                  </Link>
+                  <Link
+                    to="/admin/orderslist"
+                    className="text-gray-700 block px-4 py-2 text-sm"
+                  >
+                    Orders
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="w-[45%]">
+              <SearchBox />
+            </div>
+            <div>
+              <div className="flex gap-3">
+                <span className="hidden">
+                  <i
+                    className="fa-solid fa-magnifying-glass w-[24px] h-[24px]"
+                    onClick={handleSearch}
+                  ></i>
+                </span>
+                <div
+                  className="absolute top-[70px] right-7 xl:hidden"
+                  id="search"
+                >
+                  <SearchBox />
+                </div>
+                <span>
+                  {userInfo ? (
+                    <div className="relative inline-block text-left">
+                      <div
+                        onClick={() => setUserMenu(!userMenu)}
+                        onMouseEnter={() => setUserMenu(true)}
+                        onMouseLeave={() => setUserMenu(false)}
+                        className="hover:cursor-pointer"
+                      >
+                        <span className="text-sm font-bold py-2 ">
+                          {userInfo.name}
+                        </span>
+                      </div>
+                      {userMenu && (
+                        <div
+                          onMouseEnter={() => setUserMenu(true)}
+                          onMouseLeave={() => setUserMenu(false)}
+                          className="absolute right-0  mt-1 origin-top-right w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        >
+                          <div className="py-1">
+                            <Link
+                              to="/profile"
+                              className="text-gray-700 block px-4 py-2 text-sm"
+                            >
+                              Profile
+                            </Link>
+                            <button
+                              className="text-gray-700 block w-full px-4 py-2 text-left text-sm"
+                              onClick={signOutHandler}
+                            >
+                              Sign out
+                            </button>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  ) : (
+                    <Link to="/login">
+                      <i className="fa-regular fa-circle-user w-[24px] h-[24px]"></i>
+                    </Link>
+                  )}
+                </span>
+                <span className="xl:hidden">
+                  {userInfo && userInfo.isAdmin && (
+                    <div className="relative inline-block text-left">
+                      <div
+                        onClick={() => setAdminMenu(!adminMenu)}
+                        onMouseEnter={() => setAdminMenu(true)}
+                        onMouseLeave={() => setAdminMenu(false)}
+                        className="hover:cursor-pointer"
+                      >
+                        <span className="text-sm font-bold py-2 ">Admin</span>
+                      </div>
+                      {adminMenu && (
+                        <div
+                          onMouseEnter={() => setAdminMenu(true)}
+                          onMouseLeave={() => setAdminMenu(false)}
+                          className="absolute right-0  mt-1 origin-top-right w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        >
+                          <div className="py-1">
+                            <Link
+                              to="/admin/userlist"
+                              className="text-gray-700 block px-4 py-2 text-sm"
+                            >
+                              Users
+                            </Link>
+                            <Link
+                              to="/admin/productlist"
+                              className="text-gray-700 block px-4 py-2 text-sm"
+                            >
+                              Products
+                            </Link>
+                            <Link
+                              to="/admin/orderslist"
+                              className="text-gray-700 block px-4 py-2 text-sm"
+                            >
+                              Orders
+                            </Link>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
+        ) : (
           <div className="flex flex-row justify-between items-center">
             <div className="flex flex-row gap-2 items-center">
               <div>
@@ -125,141 +260,6 @@ const Header = () => {
                   )}
                 </span>
                 <span>
-                  {userInfo && userInfo.isAdmin && (
-                    <div className="relative inline-block text-left">
-                      <div
-                        onClick={() => setAdminMenu(!adminMenu)}
-                        onMouseEnter={() => setAdminMenu(true)}
-                        onMouseLeave={() => setAdminMenu(false)}
-                        className="hover:cursor-pointer"
-                      >
-                        <span className="text-sm font-bold py-2 ">Admin</span>
-                      </div>
-                      {adminMenu && (
-                        <div
-                          onMouseEnter={() => setAdminMenu(true)}
-                          onMouseLeave={() => setAdminMenu(false)}
-                          className="absolute right-0  mt-1 origin-top-right w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                        >
-                          <div className="py-1">
-                            <Link
-                              to="/admin/userlist"
-                              className="text-gray-700 block px-4 py-2 text-sm"
-                            >
-                              Users
-                            </Link>
-                            <Link
-                              to="/admin/productlist"
-                              className="text-gray-700 block px-4 py-2 text-sm"
-                            >
-                              Products
-                            </Link>
-                            <Link
-                              to="/admin/orderslist"
-                              className="text-gray-700 block px-4 py-2 text-sm"
-                            >
-                              Orders
-                            </Link>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  )}
-                </span>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex flex-row justify-between items-center">
-            <div className="flex flex-row gap-2 items-center">
-              <div>
-                <h1 className="text-[25.2px] font-bold text-black xl:text-[32px]">
-                  SHOP.CO
-                </h1>
-              </div>
-              <div className="hidden xl:block mx-10">
-                <div className="py-1 flex flex-row">
-                  <Link
-                    to="/admin/userlist"
-                    className="text-gray-700 block px-4 py-2 text-sm"
-                  >
-                    Users
-                  </Link>
-                  <Link
-                    to="/admin/productlist"
-                    className="text-gray-700 block px-4 py-2 text-sm"
-                  >
-                    Products
-                  </Link>
-                  <Link
-                    to="/admin/orderslist"
-                    className="text-gray-700 block px-4 py-2 text-sm"
-                  >
-                    Orders
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="w-[45%]">
-              <SearchBox />
-            </div>
-            <div>
-              <div className="flex gap-3">
-                <span className="hidden">
-                  <i
-                    className="fa-solid fa-magnifying-glass w-[24px] h-[24px]"
-                    onClick={handleSearch}
-                  ></i>
-                </span>
-                <div
-                  className="absolute top-[70px] right-7 xl:hidden"
-                  id="search"
-                >
-                  <SearchBox />
-                </div>
-                <span>
-                  {userInfo ? (
-                    <div className="relative inline-block text-left">
-                      <div
-                        onClick={() => setUserMenu(!userMenu)}
-                        onMouseEnter={() => setUserMenu(true)}
-                        onMouseLeave={() => setUserMenu(false)}
-                        className="hover:cursor-pointer"
-                      >
-                        <span className="text-sm font-bold py-2 ">
-                          {userInfo.name}
-                        </span>
-                      </div>
-                      {userMenu && (
-                        <div
-                          onMouseEnter={() => setUserMenu(true)}
-                          onMouseLeave={() => setUserMenu(false)}
-                          className="absolute right-0  mt-1 origin-top-right w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                        >
-                          <div className="py-1">
-                            <Link
-                              to="/profile"
-                              className="text-gray-700 block px-4 py-2 text-sm"
-                            >
-                              Profile
-                            </Link>
-                            <button
-                              className="text-gray-700 block w-full px-4 py-2 text-left text-sm"
-                              onClick={signOutHandler}
-                            >
-                              Sign out
-                            </button>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  ) : (
-                    <Link to="/login">
-                      <i className="fa-regular fa-circle-user w-[24px] h-[24px]"></i>
-                    </Link>
-                  )}
-                </span>
-                <span className="xl:hidden">
                   {userInfo && userInfo.isAdmin && (
                     <div className="relative inline-block text-left">
                       <div
